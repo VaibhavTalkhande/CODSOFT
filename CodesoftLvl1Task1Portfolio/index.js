1e71c8a544dd289649abef9c4404284e22722b33
VaibhavTalkhande <vaibhavtalkhande41@gmail.com>
2024-06-06 12:58:11 +0530

    portfolio done

--- CodesoftLvl1Task1Portfolio/index.js
+++ CodesoftLvl1Task1Portfolio/index.js
@@ -31,17 +31,4 @@
                 });
             });
         }); 
-       document.getElementById('contact-form').addEventListener('submit', function(event) {
-         event.preventDefault();
-            
-         emailjs.sendForm('YOUR_SERVICE_ID', 'YOUR_TEMPLATE_ID', this)
-                 .then(function() {
-                     alert('Message sent successfully!');
-                     document.getElementById('contact-form').reset();
-                 }, function(error) {
-                     alert('Failed to send message. Please try again later.');
-                     console.log('FAILED...', error);
-                 });
-         });
-
         
